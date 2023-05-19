15bd720272e7fd3c87bc1ec4aa025f8fe33f099e
Konstantin <k1st.volkov@gmail.com>
2023-05-19 13:24:43 +0300

    use manager context with the view function

--- main.py
+++ main.py
@@ -9,25 +9,23 @@ app = FastAPI(title="Quiz")
 
 
 @app.post("/quiz", response_model=QuestionOut)
-async def get_quiz_questions(questions_num: QuestionBase) -> QuestionOut:
-    db = SessionLocal()
-
-    question = None
-    while question is None:
-        response = requests.get(
-            f"https://jservice.io/api/random?count={questions_num.questions_num}"
-        )
-        questions = response.json()
-        for q in questions:
-            q_record = (
-                db.query(Question).filter(Question.question == q["question"]).first()
+async def getquizquestions(questions_num: QuestionBase) -> QuestionOut:
+    with SessionLocal() as db:
+        question = None
+        while question is None:
+            response = requests.get(
+                f"https://jservice.io/api/random?count={questions_num.questions_num}"
             )
-            if q_record is None:
-                question = Question(question=q["question"], answer=q["answer"])
-                db.add(question)
-                db.commit()
-                db.refresh(question)
-            else:
-                continue
-    db.close()
-    return QuestionOut(**question.__dict__)
+            questions = response.json()
+            for q in questions:
+                qrecord = (
+                    db.query(Question).filter(Question.question == q["question"]).first()
+                )
+                if qrecord is None:
+                    question = Question(question=q["question"], answer=q["answer"])
+                    db.add(question)
+                    db.commit()
+                    db.refresh(question)
+                else:
+                    continue
+        return QuestionOut(**question.__dict__)

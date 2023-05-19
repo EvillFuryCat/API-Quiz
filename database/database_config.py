3c8398901dc753b16207cfc92b1d04cdec8cffb0
Konstantin <k1st.volkov@gmail.com>
2023-05-19 12:30:24 +0300

    прошелся по коду black

--- database/database_config.py
+++ database/database_config.py
@@ -9,7 +9,9 @@ DATABASE_PASSWORD = os.getenv("PASSWORD")
 DATABASE_HOST = os.getenv("HOST")
 DATABASE_USER = os.getenv("USER")
 
-engine = create_engine(f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}")
+engine = create_engine(
+    f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from database.schemas import QuestionOut, QuestionBase
 import requests
 
 
-app = FastAPI(
-    title="Quiz"
-)
+app = FastAPI(title="Quiz")
+
 
 @app.post("/quiz", response_model=QuestionOut)
 async def get_quiz_questions(questions_num: QuestionBase) -> QuestionOut:
@@ -15,12 +14,16 @@ async def get_quiz_questions(questions_num: QuestionBase) -> QuestionOut:
 
     question = None
     while question is None:
-        response = requests.get(f"https://jservice.io/api/random?count={questions_num.questions_num}")
+        response = requests.get(
+            f"https://jservice.io/api/random?count={questions_num.questions_num}"
+        )
         questions = response.json()
         for q in questions:
-            q_record = db.query(Question).filter(Question.question == q['question']).first()
+            q_record = (
+                db.query(Question).filter(Question.question == q["question"]).first()
+            )
             if q_record is None:
-                question = Question(question=q['question'], answer=q['answer'])
+                question = Question(question=q["question"], answer=q["answer"])
                 db.add(question)
                 db.commit()
                 db.refresh(question)

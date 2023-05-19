b323939eeb9fba4b968b5b612f767e6880185373
Konstantin <k1st.volkov@gmail.com>
2023-05-19 12:29:10 +0300

    add .env

--- database/database_config.py
+++ database/database_config.py
@@ -4,12 +4,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-DATABASE_HOST = os.getenv("DATABASE_HOST")
-DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_NAME = os.getenv("NAME")
+DATABASE_PASSWORD = os.getenv("PASSWORD")
+DATABASE_HOST = os.getenv("HOST")
+DATABASE_USER = os.getenv("USER")
 
-engine = create_engine("postgresql+psycopg2://postgres:postgres@db/postgres")
+engine = create_engine(f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
